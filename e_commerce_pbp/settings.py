9089ff407f8befe9653a4e5602de91e33a250bc2
Valentino <valentino.kim@ui.ac.id>
2024-09-07 02:24:10 +0700

    git ignore

--- e_commerce_pbp/settings.py
+++ e_commerce_pbp/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-(com(cg!aoe5qa7@v##nhd+a0ff(=&ytooh*gey-28aj23ybe$
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
 
 
 # Application definition
