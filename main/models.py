ac3f60879259a79f6c73440831f3e96509c8f936
Valentino <valentino.kim@ui.ac.id>
2024-09-13 20:17:21 +0700

    ngasih gambar uni bakwan

--- main/models.py
+++ main/models.py
@@ -4,6 +4,7 @@ class Item(models.Model):
     name = models.CharField(max_length=100)
     price = models.IntegerField()
     description = models.TextField()
+    image = models.ImageField()
 
     def __str__(self):
         return self.name
--- main/views.py
+++ main/views.py
@@ -5,11 +5,13 @@ def show_home(request):
         'nama': 'Valentino Kim Fernando',  
         'kelas': 'PBP F',
         'items': [
-            {'name': 'Es Kulkul Uni Bakwan', 'price': 70.000, 'description': 'Es Kulkul Uni Bakwan, es asli dari Uni Bakwan Onde Mande', \
-             'image': 'static/images/unibakwan.png'},
-            {'name': 'Ayam Goreng Bunda Mewing', 'price': 25.000, 'description': 'Merupakan salah satu ayam goreng langka yang dibuat oleh \
-             bunda yang mewing.', 'image': 'static/images/mewing.png'},
-            # Add more items as needed
+            {'name': 'Es Kulkul Uni Bakwan', 'price': 70, 'description': 'Es Kulkul Uni Bakwan, es asli dari Uni Bakwan Onde Mande', \
+             'image': 'https://i.pinimg.com/736x/42/b1/22/42b12243ca70053a7c6f22241787f271.jpg'},
+            {'name': 'Ayam Goreng Bunda Mewing', 'price': 25, 'description': 'Merupakan salah satu ayam goreng langka yang dibuat oleh \
+             bunda yang mewing.', 'image': 'https://steamuserimages-a.akamaihd.net/ugc/2270441744978599352/3A97A06E3670819C1E5A4525DED7FBF4DAC5DE21/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false'},
+            {'name': 'Odading ', 'price': 25, 'description': 'Merupakan salah satu ayam goreng langka yang dibuat oleh \
+             bunda yang mewing.', 'image': 'https://steamuserimages-a.akamaihd.net/ugc/2270441744978599352/3A97A06E3670819C1E5A4525DED7FBF4DAC5DE21/?imw=512&&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false'},
+            
         ],
     }
     return render(request, 'home.html', context)
