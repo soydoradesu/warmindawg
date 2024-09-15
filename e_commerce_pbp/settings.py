1e66a57459c957cf4410fe263ad791df748bfe91
Valentino <valentino.kim@ui.ac.id>
2024-09-15 11:55:44 +0700

    coba redeploy

--- e_commerce_pbp/settings.py
+++ e_commerce_pbp/settings.py
@@ -122,7 +122,7 @@ STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 STATICFILES_DIRS = [
-    BASE_DIR / 'main/static',
+    BASE_DIR / 'static',
 ]
 
 # Default primary key field type
