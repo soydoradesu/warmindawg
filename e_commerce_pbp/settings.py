95486093ae695d0f70877cd1c7f9daca6575d4cf
Valentino <valentino.kim@ui.ac.id>
2024-10-02 13:39:56 +0700

    fixing deploy(2)

--- e_commerce_pbp/settings.py
+++ e_commerce_pbp/settings.py
@@ -119,12 +119,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # Dokumentasi: https://docs.djangoproject.com/en/5.1/howto/static-files/
 STATIC_URL = '/static/'
+
 if DEBUG:
     STATICFILES_DIRS = [
         BASE_DIR / 'static' # merujuk ke /static root project pada mode development
     ]
     
-STATIC_ROOT = BASE_DIR / 'static' # merujuk ke /static root project pada mode production
+STATIC_ROOT = BASE_DIR / 'staticfiles' # merujuk ke /static root project pada mode production
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
