db2a7ff067d37f7b79f98220782a66c4219330be
Valentino <valentino.kim@ui.ac.id>
2024-10-02 13:09:26 +0700

    deploying fix

--- e_commerce_pbp/settings.py
+++ e_commerce_pbp/settings.py
@@ -123,8 +123,8 @@ if DEBUG:
     STATICFILES_DIRS = [
         BASE_DIR / 'static' # merujuk ke /static root project pada mode development
     ]
-else:
-    STATIC_ROOT = BASE_DIR / 'static' # merujuk ke /static root project pada mode production
+    
+STATIC_ROOT = BASE_DIR / 'static' # merujuk ke /static root project pada mode production
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
